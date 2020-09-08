6fc7171d2ecbd708ec21e7f908caa0949da6ab0f
Vic Flores <vicsito2014@gmail.com>
2020-09-07 23:30:52 -0600

    Was changed filed build for docs

---
+++ docs/precache-manifest.46c090e713f330044721844144e53f99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e726c3dd232a80ad477e69a720e7e4a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ac4c3546504742ed13",
+    "url": "/static/js/2.9575853e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9575853e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59155bc460abb6725a9",
+    "url": "/static/js/main.ee87f3e2.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
